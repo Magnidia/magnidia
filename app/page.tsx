fd54abd58cee340595ac0d8e235f158fc0da8949
Ian Wood <ianwood103@gmail.com>
2024-01-30 17:53:14 -0500

    load change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getEvents } from "@/utils/event";
 import { Event } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const events: Event[] = await getEvents();
 
