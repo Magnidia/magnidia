6a68cdcf91f9dbd05bf428c40fc3b60b26442d27
Ian Wood <ianwood103@gmail.com>
2024-03-07 11:13:05 -0500

    added url encoding and made build compatible

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,51 +1,53 @@
-import { NextResponse } from "next/server";
-import { S3Client, PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
+import { NextRequest, NextResponse } from "next/server";
+import {
+  S3Client,
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY!,
-      secretAccessKey: process.env.AWS_SECRET_KEY!,
-    },
-  });
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_KEY!,
+  },
+});
 
 async function uploadFileToS3(file: Buffer, fileName: string): Promise<string> {
-    const fileBuffer = file;
-    console.log(fileName);
+  const encodedFileName = encodeURIComponent(fileName);
 
-    const params: PutObjectCommandInput = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME!,
-      Key: `${fileName}`,
-      Body: fileBuffer,
-      ContentType: 'image/png',
-    };
-  
-    const s3Client = new S3Client(); // Instantiate your S3Client
-  
-    const command = new PutObjectCommand(params);
-    await s3Client.send(command);
-  
-    const objectUrl = `https://magnidia-imageupload.s3.us-east-2.amazonaws.com/${params.Key}`;
+  const params: PutObjectCommandInput = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME!,
+    Key: encodedFileName,
+    Body: file,
+    ContentType: "image/png",
+  };
 
-    return objectUrl;
+  const s3Client = new S3Client(); // Instantiate your S3Client
 
+  const command = new PutObjectCommand(params);
+  await s3Client.send(command);
+
+  const objectUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${encodedFileName}`;
+
+  return objectUrl;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData: FormData = await request.formData();
+    const file = formData.get("file") as File;
+
+    if (file) {
+      const buffer = Buffer.from(await file.arrayBuffer());
+      const objectUrl = await uploadFileToS3(buffer, file.name);
+
+      return NextResponse.json({ success: true, objectUrl }, { status: 201 });
+    } else {
+      return NextResponse.json({ error: "File is required." }, { status: 400 });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error });
   }
-
-export async function POST(request: { formData: () => Promise<any> }) {
-	try {
-
-		const formData = await request.formData();
-		const file = formData.get('file');
-
-		if(!file) {
-			return NextResponse.json( { error: "File is required."}, { status: 400 } );
-		} 
-
-		const buffer = Buffer.from(await file.arrayBuffer());
-		const objectUrl = await uploadFileToS3(buffer, file.name);
-
-		return NextResponse.json({ success: true, objectUrl});
-	} catch (error) {
-		return NextResponse.json({ error });
-	}
 }
