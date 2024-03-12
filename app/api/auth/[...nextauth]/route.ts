20f30d1b59a63efa6580ffda331666aab4be3ec4
Ian Wood <ianwood103@gmail.com>
2024-03-12 16:35:51 -0400

    add nextauth secret and fix formatting

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,24 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import db from '@/utils/db';
+import db from "@/utils/db";
 
-const authOptions : AuthOptions = {
-    adapter: PrismaAdapter(db),
-        providers: [
-            GoogleProvider({
-                clientId: process.env.GOOGLE_CLIENT_ID as string,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-                checks: ['none'],
-            })
-        ],
-        session: {
-            strategy: 'jwt',
-        },
-       
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      checks: ["none"],
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
-    
-}
+const handler = NextAuth(authOptions);
 
-const handler =  NextAuth(authOptions);
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
