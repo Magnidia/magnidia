a78790b4b33e3432ff21e33daeaf461dbef5a4b5
Tejas Vermani <tejasvermani@gmail.com>
2024-03-13 11:54:44 -0400

    fixed prisma seed and database clear functionality

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.user.create({
+async function clearDatabase() {
+  await prisma.ticket.deleteMany();
+  await prisma.event.deleteMany();
+  await prisma.user.deleteMany();
+}
+
+
+async function seedDatabase() {
+  // create user entries first because they are used in event and ticket entries.
+  const testUser = await prisma.user.create({
     data: {
       email: 'alice@example.com',
       name: 'Alice',
@@ -11,7 +19,47 @@ async function main() {
       address: '123 Main St',
     },
   });
-  console.log(`Created user with id: ${user.id}`);
+  console.log(`Created user with id: ${testUser.id}`);
+
+  const event1 = await prisma.event.create({
+    data: {
+      name: 'Dummy Event 1',
+      userId: testUser.id,
+      images: [
+        "https://magnidia-imageupload.s3.us-east-2.amazonaws.com/ATL+Banner.jpeg"
+      ],
+      date: new Date()
+    },
+  });
+
+  const event2 = await prisma.event.create({
+    data: {
+      name: 'Dummy Event 2',
+      userId: testUser.id,
+      images: [
+        "https://magnidia-imageupload.s3.us-east-2.amazonaws.com/demo.png"
+      ],
+      date: new Date()
+    },
+  });
+
+  const event3 = await prisma.event.create({
+    data: {
+      name: 'Dummy Event 3',
+      userId: testUser.id,
+      images: [
+        "https://magnidia-imageupload.s3.us-east-2.amazonaws.com/GT+Header+2.jpeg"
+      ],
+      date: new Date()
+    },
+  });
+
+
+}
+
+async function main() {
+  await clearDatabase();
+  await seedDatabase();
 }
 
 main()
