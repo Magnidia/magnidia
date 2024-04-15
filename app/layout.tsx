5d75c7b192c25c776aa3db14ee0f4ad3c33ef6e6
Yasharacha <yasharacha@gmail.com>
2024-04-15 17:14:55 -0400

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import fonts from "@/utils/fonts";
 import "./globals.css";
+import Nav from "@/components/Nav";
 
 export const metadata: Metadata = {
   title: "Magnidia",
@@ -14,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={fonts}>{children}</body>
+      <body 
+        className={fonts}> <Nav /> {children}
+      
+      </body>
+
     </html>
   );
 }
