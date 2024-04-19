ecf6aea7925f93ce5ca99668b7b8676f68bc5ca6
Ian Wood <ianwood103@gmail.com>
2024-04-19 18:16:43 -0400

    update navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import styles from "./Nav.module.css";
-import Image from "public/logo.png";
 import Link from "next/link";
 
-const Nav: React.FC<{}> = () => {
+const Nav: React.FC = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles["logo-container"]}>
-        <Link href="/">
-          <img src="/logo.png" alt="Logo" />
-        </Link>
-      </div>
-
-      <div>
-        <Link
-          href="/create-event"
-          className="absolute right-10 mt-4 hover:text-primary"
-        >
-          Create Event
-        </Link>
+    <nav className="fixed left-2/4 top-10 -translate-x-2/4 w-11/12 rounded-full bg-white z-50 shadow-2xl p-5">
+      <div className="flex flex-row">
+        <div className="w-1/6 min-w-44 ml-5">
+          <Link href="/">
+            <img src="/logo.png" alt="Logo" />
+          </Link>
+        </div>
+        <div className="flex flex-row justify-end items-center w-full mr-5 gap-7">
+          <Link href="/" className="text-sm font-semibold">
+            Home
+          </Link>
+          <Link href="/create-event" className="text-sm font-semibold">
+            Create Event
+          </Link>
+          <Link href="/authtest" className="text-sm font-semibold">
+            My Account
+          </Link>
+        </div>
       </div>
     </nav>
   );
