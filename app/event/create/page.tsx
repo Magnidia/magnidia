2ac9b02ab87d38a34e50ada283e47e72ff5cfb5f
Ian Wood <ianwood103@gmail.com>
2024-05-01 22:42:17 -0400

    final touches on event creation screen

--- app/event/create/page.tsx
+++ app/event/create/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import Map from "@/components/Map";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
 import { Calendar, Input, Textarea, Button } from "@rewind-ui/core";
-import { IoMdCloudUpload } from "react-icons/io";
 import { isValid, format } from "date-fns";
 
-interface FormData {
+interface EventData {
   name: string;
   date: Date;
   startTime: Date;
@@ -15,14 +15,15 @@ interface FormData {
   city: string;
   state: string;
   description: string;
-  images: string[];
+  imageUrl: string;
   latitude: number;
   longitude: number;
   price: number;
+  userId: string;
 }
 
 const CreateEventPage = () => {
-  const initialFormData: FormData = {
+  const initialEventData: EventData = {
     name: "",
     date: new Date(),
     startTime: new Date(),
@@ -31,19 +32,42 @@ const CreateEventPage = () => {
     city: "",
     state: "",
     description: "",
-    images: [],
+    imageUrl: "",
     latitude: 0,
     longitude: 0,
     price: 0,
+    userId: "clv1g1vre0000438yjm6fj9fc",
   };
 
-  const [formData, setFormData] = useState<FormData>(initialFormData);
+  const router = useRouter();
+
+  const [eventData, setEventData] = useState<EventData>(initialEventData);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const setCoordinates = async (address: string) => {
+    try {
+      const response = await fetch(
+        `/api/geocode/${encodeURIComponent(address)}`
+      );
+      const data = await response.json();
+      if (data) {
+        const { lat, lng } = data;
+        setEventData((prevState) => ({
+          ...prevState,
+          latitude: lat,
+          longitude: lng,
+        }));
+      }
+    } catch (error) {
+      console.error("Unable to retrieve coordinates.", error);
+    }
+  };
 
   const handleChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = event.target;
-    setFormData((prevState) => ({
+    setEventData((prevState) => ({
       ...prevState,
       [name]: value,
     }));
@@ -51,10 +75,10 @@ const CreateEventPage = () => {
 
   const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    const newDate = new Date(formData.date);
+    const newDate = new Date(eventData.date);
     const [hours, minutes] = value.split(":").map(Number);
     newDate.setHours(hours, minutes);
-    setFormData((prevState) => ({
+    setEventData((prevState) => ({
       ...prevState,
       [name]: newDate,
     }));
@@ -62,14 +86,45 @@ const CreateEventPage = () => {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    console.log("Form Data Submitted:", formData);
-    setFormData(initialFormData);
+    const formData = new FormData();
+    if (
+      !imageInputRef.current?.files ||
+      imageInputRef.current?.files.length === 0
+    ) {
+      alert("Please upload an image.");
+      return;
+    }
+    formData.append("file", imageInputRef.current?.files[0]);
+    const res = await fetch("/api/image", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    setEventData((prevState) => ({
+      ...prevState,
+      imageUrl: data.imageUrl,
+    }));
+
+    try {
+      const response = await fetch("/api/event", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...eventData, imageUrl: data.imageUrl }),
+      });
+      const newEvent = await response.json();
+      setEventData(initialEventData);
+      router.push(`/event/${newEvent.eventId}`);
+    } catch (error) {
+      console.error("Unable to create event.", error);
+    }
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-lighterBlue pt-44">
       <div className="rounded-md border border-black bg-white w-10/12 mb-20">
-        {formData && (
+        {eventData && (
           <div className="flex flex-col">
             <div className="flex flex-row flex-wrap lg:flex-nowrap p-10 gap-10">
               <div className="flex flex-col w-full">
@@ -78,55 +133,66 @@ const CreateEventPage = () => {
                   type="text"
                   placeholder="NAME"
                   className="p-1 mt-4"
-                  value={formData.name}
+                  value={eventData.name}
                   onChange={handleChange}
                   name="name"
                 />
                 <span className="font-bold text-lg mt-8">DESCRIPTION:</span>
                 <Textarea
                   placeholder="DESCRIPTION"
-                  className="h-72 mt-4 pt-3"
-                  value={formData.description}
+                  className="h-64 mt-4 pt-3"
+                  value={eventData.description}
                   onChange={handleChange}
                   name="description"
                   size="lg"
                 />
-                <Button
-                  className="w-full m-auto mt-8"
-                  color="gray"
-                  size="lg"
-                  shadow="base"
-                >
-                  UPLOAD IMAGE
-                  <IoMdCloudUpload className="text-xl inline-block ml-3" />
-                </Button>
+                <span className="font-bold text-lg mt-8">COVER PHOTO:</span>
+                <Input
+                  color="blue"
+                  type="file"
+                  accept="image/*"
+                  ref={imageInputRef}
+                  className="w-full m-auto mt-4"
+                />
               </div>
               <div className="flex flex-col w-full">
                 <span className="font-bold text-lg">SET LOCATION:</span>
                 <Input
                   placeholder="ADDRESS"
                   className="p-1 mt-4"
-                  value={formData.address}
+                  value={eventData.address}
                   onChange={handleChange}
                   name="address"
                 />
                 <Input
                   placeholder="CITY"
                   className="p-1 mt-4"
-                  value={formData.city}
+                  value={eventData.city}
                   onChange={handleChange}
                   name="city"
                 />
-                <Input
-                  placeholder="STATE"
-                  className="p-1 mt-4"
-                  value={formData.state}
-                  onChange={handleChange}
-                  name="state"
-                />
+                <div className="flex flex-row items-center gap-5">
+                  <Input
+                    placeholder="STATE"
+                    className="p-1 mt-4"
+                    value={eventData.state}
+                    onChange={handleChange}
+                    name="state"
+                  />
+                  <Button
+                    className="mt-4"
+                    onClick={() => {
+                      setCoordinates(
+                        `${eventData.address}, ${eventData.city}, ${eventData.state}`
+                      );
+                    }}
+                  >
+                    CONFIRM
+                  </Button>
+                </div>
                 <Map
-                  latitude={formData.latitude}
-                  longitude={formData.longitude}
+                  lat={eventData.latitude}
+                  lng={eventData.longitude}
                   styles={{
                     width: "100%",
                     height: "190px",
@@ -141,8 +207,14 @@ const CreateEventPage = () => {
                     placeholder="PRICE"
                     className="p-1 mt-4"
                     type="number"
-                    value={formData.price}
-                    onChange={handleChange}
+                    value={eventData.price}
+                    onChange={(event) => {
+                      const { name, valueAsNumber } = event.target;
+                      setEventData((prevState) => ({
+                        ...prevState,
+                        [name]: valueAsNumber,
+                      }));
+                    }}
                     name="price"
                   />
                   <span className="font-bold text-lg mt-4 ml-4">USD</span>
@@ -151,10 +223,10 @@ const CreateEventPage = () => {
               <div className="flex flex-col w-full">
                 <span className="font-bold text-lg">SET DATE:</span>
                 <Calendar
-                  value={formData.date}
+                  value={eventData.date}
                   onChange={(date) => {
                     if (date && isValid(date)) {
-                      setFormData((prevState) => ({
+                      setEventData((prevState) => ({
                         ...prevState,
                         date,
                       }));
@@ -170,7 +242,7 @@ const CreateEventPage = () => {
                   <Input
                     className="p-1 mt-4"
                     type="time"
-                    value={format(formData.startTime, "HH:mm")}
+                    value={format(eventData.startTime, "HH:mm")}
                     onChange={handleTimeChange}
                     name="startTime"
                   />
@@ -178,7 +250,7 @@ const CreateEventPage = () => {
                   <Input
                     className="p-1 mt-4"
                     type="time"
-                    value={format(formData.endTime, "HH:mm")}
+                    value={format(eventData.endTime, "HH:mm")}
                     onChange={handleTimeChange}
                     name="endTime"
                   />
