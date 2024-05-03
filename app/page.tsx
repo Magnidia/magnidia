c771f8efbcc507013cd2e06043bc0ddf2d17eac5
Ian Wood <ianwood103@gmail.com>
2024-05-03 02:29:13 -0400

    create landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+"use client";
 
-export default async function Home() {
+import { Button } from "@rewind-ui/core";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
   return (
-    <div className="w-full flex flex-row justify-center">
-      <h1 className="text-4xl font-bold mt-20">Landing Page</h1>
+    <div className="w-full max-h-full">
+      <div className="w-full flex flex-row items-center justify-between p-8">
+        <Image
+          src={"/logo.png"}
+          width={300}
+          height={0}
+          alt="Logo"
+          className="z-10"
+        />
+        <div className="flex flex-row z-10 gap-5">
+          <Button
+            color="blue"
+            shadow="base"
+            onClick={() => signIn("google", { callbackUrl: "/feed" })}
+          >
+            Sign Up
+          </Button>
+          <Button
+            color="gray"
+            shadow="base"
+            onClick={() => signIn("google", { callbackUrl: "/feed" })}
+          >
+            Sign In
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center py-20 px-60">
+        <h1 className="text-7xl text-[#363636] z-10 font-bold">
+          Making planning and attending events{" "}
+          <span className="text-primary">a breeze.</span>
+        </h1>
+      </div>
+      <Image
+        alt="Event Image"
+        src="/landing.jpg"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full h-auto absolute top-0 left-0"
+      ></Image>
     </div>
   );
 }
