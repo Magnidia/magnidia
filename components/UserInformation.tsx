8e3681a3a7e2f918d37d54f0438a745c1b21b269
Ian Wood <ianwood103@gmail.com>
2024-05-06 01:57:25 -0400

    add ticket information

--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -2,11 +2,16 @@
 
 import { FC, useState } from "react";
 import { Button } from "@rewind-ui/core";
-import { Ticket, Event } from "@prisma/client";
+import { Event, Prisma } from "@prisma/client";
 import EventCard from "./EventCard";
+import MyTicketCard from "./MyTicketCard";
+
+type TicketWithEvent = Prisma.TicketGetPayload<{
+  include: { event: true };
+}>;
 
 interface UserInformationProps {
-  tickets: Ticket[];
+  tickets: TicketWithEvent[];
   events: Event[];
 }
 
@@ -40,10 +45,13 @@ const UserInformation: FC<UserInformationProps> = ({
           My Events
         </Button>
       </div>
-      <div className="w-full flex flex-col items-center">
+      <div className="w-full flex flex-col items-start p-10 gap-5">
         {!showTickets &&
           events.map((event) => <EventCard event={event} key={event.id} />)}
-        {showTickets && tickets.map((ticket) => <p>{ticket.id}</p>)}
+        {showTickets &&
+          tickets.map((ticket) => (
+            <MyTicketCard ticket={ticket} key={ticket.id} />
+          ))}
       </div>
     </>
   );
