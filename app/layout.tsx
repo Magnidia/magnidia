3b25aa0b205269e50dd92827768fd46446cb1c98
Ian Wood <ianwood103@gmail.com>
2024-05-07 01:16:54 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Magnidia",
   description: "Event Ticketing and Management Platform",
+  icons: {
+    icon: "/logo_square.png",
+  },
 };
 
 export default async function RootLayout({
